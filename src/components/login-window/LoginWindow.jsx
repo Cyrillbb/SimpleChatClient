ca9a39d62493352dd58e5158dfe4543eaa923dc4
Fallopio <kibaryshnikov@yandex.ru>
2020-07-21 09:38:16 +0400

    fixed login and registration

--- src/components/login-window/LoginWindow.jsx
+++ src/components/login-window/LoginWindow.jsx
@@ -7,10 +7,8 @@ import './LoginWindow.css'
 
 function LoginWindow(props) {
     const { socket } = props;
-
     const [nickname, setNickname] = useState('');
     const [password, setPassword] = useState('');
-    const [loginType, setLoginType] = useState('login');
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -18,59 +16,38 @@ function LoginWindow(props) {
     };
 
     const handleCreate = (e) => {
-        e.preventDefault();
         socket.emit('newUser', { nickname, password });
-        setLoginType('login');
         setNickname('');
         setPassword('');
     };
 
     return (
         <div>
-            {loginType === 'login' ?
-                <form className="loginForm" onSubmit={handleLogin}>
-                    <h3>Simple chat login</h3>
-                    <input className='loginInpt'
-                        type="text"
-                        onChange={(e) => { setNickname(e.target.value) }}
-                        placeholder='enter ur nickname'
-                        id="nick" required
-                    />
-                    <input className='loginInpt'
-                        type="password"
-                        placeholder='enter ur password'
-                        id="pass" required
-                        onChange={e => { setPassword(e.target.value) }}
-                    />
-                    <div className='btnGroup'>
-                        <button className='formBtn' type='submit'>Login</button>
-                        <button className='formBtn'
-                            type='button'
-                            onClick={() => { setLoginType('reg'); }}>
-                            Create account
-                        </button>
-                    </div>
-                </form> :
-                <form onSubmit={handleCreate} className="loginForm">
-                    <h3>Simple chat registration</h3>
-                    <input className='loginInpt'
-                        type="text"
-                        id='nick'
-                        placeholder='enter ur nickname'
-                        onChange={e => { setNickname(e.target.value) }} required
-                    />
-                    <input
-                        className='loginInpt'
-                        type="password" id='pass'
-                        placeholder='enter ur password'
-                        onChange={e => { setPassword(e.target.value) }} required
-                    />
-                    <div className='btnGroup'>
-                        <button className='formBtn' type='submit'>Create account</button>
-                        <button className='formBtn' onClick={() => setLoginType('login')}>Back</button>
-                    </div>
-                </form>
-            }
+            <form className="loginForm" onSubmit={handleLogin}>
+                <h3>Simple chat login</h3>
+                <input className='loginInpt'
+                    type="text"
+                    value={nickname}
+                    onChange={(e) => { setNickname(e.target.value) }}
+                    placeholder='enter ur nickname'
+                    id="nick" required
+                />
+                <input className='loginInpt'
+                    type="password"
+                    placeholder='enter ur password'
+                    id="pass" required
+                    value={password}
+                    onChange={e => { setPassword(e.target.value) }}
+                />
+                <div className='btnGroup'>
+                    <button className='formBtn' type='submit'>Login</button>
+                    <button className='formBtn'
+                        type='button'
+                        onClick={handleCreate}>
+                        Create account
+                    </button>
+                </div>
+            </form>
         </div>
     )
 };
